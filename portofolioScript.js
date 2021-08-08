7950a752acf8213541a2231cc110f0590172b051
rohan821101 <79596636+rohan821101@users.noreply.github.com>
2021-08-08 21:03:10 +0530

    Add files via upload

---
+++ portofolioScript.js
@@ -0,0 +1,21 @@
+//for about...
+function aboutB1(){
+    
+}
+function aboutB2(){
+    
+}
+function aboutB3(){
+    
+}
+function aboutB4(){
+    
+}
+
+//for projects...
+function projectsB1(){
+
+}
+function projectsB2(){
+
+}
